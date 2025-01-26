e4a9ddd74224c649315927318646608d819436e2
Nurbolat Amilbek <n.amilbek@efactoring.kz>
2025-01-26 14:46:57 +0100

    first backend commit

---
+++ quiztopia-backend/src/main/java/kz/project/quiztopia/quiztopiabackend/QuiztopiaBackendApplication.java
@@ -0,0 +1,13 @@
+package kz.project.quiztopia.quiztopiabackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class QuiztopiaBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(QuiztopiaBackendApplication.class, args);
+    }
+
+}
---
+++ quiztopia-backend/src/test/java/kz/project/quiztopia/quiztopiabackend/QuiztopiaBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package kz.project.quiztopia.quiztopiabackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class QuiztopiaBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
